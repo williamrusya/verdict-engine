65d62b25991e7d3cea90a50bb4f20cc712b55d6b
rustemospanov <rustem.o@halykmarket.com>
2025-12-26 13:28:13 +0500

    Initial commit

---
+++ src/main/java/rustemospanov/verdictengine/VerdictEngineApplication.java
@@ -0,0 +1,13 @@
+package rustemospanov.verdictengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdictEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerdictEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rustemospanov/verdictengine/VerdictEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package rustemospanov.verdictengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdictEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
